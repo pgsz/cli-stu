36d35e30bffbe4678bb426ea816532064acc7e08
pgsz <962797832@qq.com>
2022-03-25 11:43:14 +0800

    pg-cli-demo 版本

--- README.md
+++ README.md
@@ -1,29 +1,18 @@
-### V1
-
-- 用户输入命令，这边创建项目
-  - 使用[commander.js](https://github.com/tj/commander.js/blob/master/Readme_zh-CN.md)，解析用户的目录，提取用户的输入给脚手架
-  - 使用 `npm link` 注册成全局命令，这样可以在电脑任何地方适应命令；可用 `npm ls -g -depth=0` 查看已注册的全局命令，可以使用 `npm unlink` 取消全局注册或 `npm rm -g 模块名`进行删除
-
-- 脚手架解析用户命令，弹出交互语句，询问用户创建项目所需功能
-  - 使用[Inquirer.js](https://github.com/SBoudrias/Inquirer.js/)，弹出问题和选项，让用户选择
-  
-- 用户选择所需功能
-  
-- 脚手架根据用户的选择创建 `package.json` 文件，并添加对应的依赖项
-  
-- 脚手架根据用户的选择渲染项目模板，生成文件
-  
-- 执行 `npm install` 命令安装依赖
+[commander：](https://github.com/tj/commander.js/blob/master/Readme_zh-CN.md0)命令配置工具
+[chalk：](https://github.com/chalk/chalk)命令行美化工具
+[inquirer：](https://github.com/SBoudrias/Inquirer.js)命令行交互工具
+[ora：](https://github.com/sindresorhus/ora)命令行 loading 效果
+[fs-extra：](https://github.com/jprichardson/node-fs-extra)更友好的文件操作
+[download-git-repo：](https://gitlab.com/flippidippi/download-git-repo)命令行下载工具
+[figlet：](https://github.com/patorjk/figlet.js)生成基于 ASCII 的艺术字
 
 
-### V2
+[学习来源](https://juejin.cn/post/7077717940941881358)
 
-- 创建项目时判断该项目是否已存在，支持覆盖和合并创建
 
-- 选择功能时提供默认配置和手动选择两种模式
-
-- 如果用户的环境同时存在 yarn 和 npm，则提示用户使用哪个包管理器
-
-- 如果包管理器的默认源速度慢，则提示用户是否切换到淘宝源
-
-- 如果是手动选择功能，则在结束之后询问用户是否将本次的选择保存为默认配置
+`package.json` 中：`pg-cli` 命令代替 `node ./bin/index.js`
+```json
+"bin": {
+  "pg-cli": "./bin/index.js"
+},
+```
--- bin/index.js
+++ bin/index.js
@@ -1,17 +1,54 @@
 #!/usr/bin/env node
-// 全局命令
+/**
+ * 首行
+ * #! 标识作用，文件可以当做脚本来运行，指定脚本的解析程序
+ * /usr/bin/env 用户(usr)的安装根目录(bin)下的env环境变量中
+ * node 用 node 来执行此文件
+ * 发 npm 包时，需要在入口文件指定该指令，否则会抛出 No such file or directory 错误
+*/
+
 const program = require('commander')
-const create = require('../lib/create')
+const chalk = require('chalk')
+const figlet = require('figlet')
 
-// 版本号
-program.version(require('../package.json').version)
-
-// 处理用户命令
 program
-.command('create <name>')
-.description('create a new project')
-.action(name => {
-    create(name)
+  .name('pg-cli')
+  .usage(`<command> [option]`)
+
+// package.json 中存取了项目的版本号 version
+// 直接使用该属性
+program.version(`pg-cli ${require("../package.json").version}`);
+
+program
+  .command("create <project-name>")
+  .description("create a new project")
+  .option("-f, --force", "overwrite target directory if it exists")
+  .action((projectName, options) => {
+    require("../lib/create")(projectName, options)
+  })
+
+program
+  .command("config [value]")
+  .description("inspect and modify the config")
+  .option("-g --get <key>", "get value by key")
+  .option("-s, --set <key> <value>", "set option[key] is value")
+  .option("-d, --delete <key>", "delete option by key")
+  .action((value, keys) => {
+    console.log(value, keys)
+  })
+
+program.on("--help", () => {
+  console.log(
+    "\r\n" + figlet.textSync("pg-cli", {
+      font: "3D-ASCII",
+      horizontalLayout: 'default',
+      verticalLayout: 'default',
+      width: 80,
+      whitespaceBreak: true
+    })
+  )
+  console.log(` Run ${chalk.cyan('pg-cli <command> --help')} for detailed usage of given command.`)
+  console.log()
 })
 
-program.parse()
+program.parse(process.argv);
